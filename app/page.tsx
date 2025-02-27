4d93e6ba0cf198048fb2daaeeabf78acd3eccfd2
dapaa67 <dff6768@gmail.com>
2025-02-27 23:17:03 +0700

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,69 @@
+import Image from "next/image";
+import { socialLinks } from "./config";
+
+export default function Page() {
+  return (
+    <section>
+      <a href={socialLinks.twitter} target="_blank">
+        <Image
+          src="/photo.png"
+          alt="Profile photo"
+          className="rounded-full bg-gray-100 block lg:mt-5 mt-0 lg:mb-5 mb-10 mx-auto sm:float-right sm:ml-5 sm:mb-5 grayscale hover:grayscale-0"
+          unoptimized
+          width={160}
+          height={160}
+          priority
+        />
+      </a>
+      <h1 className="mb-8 text-2xl font-medium tracking-tight">
+        Muhammad Daffa
+      </h1>
+      <div className="prose prose-neutral dark:prose-invert">
+        <p>
+          A clean, fast, and lightweight portfolio template built with Next.js,
+          Vercel, and Tailwind CSS.
+        </p>
+        <p>
+          Nextfolio has everything you need for a portfolio: MDX blog, SEO, RSS,
+          Atom & JSON feeds, analytics, Tweet & YouTube embeds, KaTeX and {""}
+          <a
+            target="_blank"
+            href="https://github.com/1msirius/Nextfolio?tab=readme-ov-file#features"
+          >
+            more
+          </a>
+          .
+        </p>
+        <p>
+          Nextfolio is{" "}
+          <a href={socialLinks.github} target="_blank">
+            open-source
+          </a>{" "}
+          and fully customizable, making it easy to add more features.
+        </p>
+        <p>
+          <a
+            href="https://vercel.com/new/clone?repository-url=https%3A%2F%2Fgithub.com%2F1msirius%2FNextfolio"
+            target="_blank"
+          >
+            Deploy
+          </a>{" "}
+          your Nextfolio site with Vercel in minutes and follow the set up
+          instructions in the{" "}
+          <a href="/blog/getting-started">Getting Started</a> post.
+        </p>
+        <p>
+          Built and maintained by{" "}
+          <a href="https://imsirius.xyz/" target="_blank">
+            Sirius 
+          </a>
+           Thank a lot by {""}
+          <a href="https://www.youtube.com/watch?v=vXBw1vQ0O-g&list=PLPanbgyToztYlSxKLf7qFl6VpLYn2T_i1" target="_blank">
+          me
+          </a>
+          .
+        </p>
+      </div>
+    </section>
+  );
+}
